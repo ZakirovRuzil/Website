399521bf1fe67cfb300140ef746b7fc15ec8eefe
Ruzil Zakirov <ruzyzakov@gmail.com>
2023-12-29 17:53:41 +0300

    [add] Second push

--- Web/Controllers/BossController.cs
+++ Web/Controllers/BossController.cs
@@ -1,56 +1,146 @@
 ﻿using System.Text;
 using Web.Attributes;
+using Web.Constants;
 using Web.ORM.Entities;
 using Web.ORM.Repository;
 
 namespace Web.Controllers;
 
-[HttpController(name: "Boss")]
-public class BossController
+[HttpController(name: "home")]
+public class MainPageController
 {
-    private static BossesRepository _repo = new BossesRepository();
+    private static BossesRepository _bosses = new BossesRepository();
+    private static LevelsRepository _levels = new LevelsRepository();
+    private static FriendsRepository _friends = new FriendsRepository();
 
-    [Get("GetBosses")]
-    public string GetBosses()
+    [Get("GetMainPage")]
+    public string GetMainPage()
     {
-        var bosses = _repo.Select().Result;
-
-        var html = new StringBuilder();
-
-        foreach (var boss in bosses.OrderBy(b => b.Id))
+        var bossesHtml = GetBossesHtml();
+        var levelsHtml = GetLevelsHtml();
+        var friendHtml = GetFriendsHtml();
+        
+        var globalPlaceholders = new Dictionary<string, string>()
         {
-            html.Append($"<p> Boss name: {boss.Name}</p>" +
-                        $"<p style=\"display: flex;\"> " +
-                        $"Boss picture:\n " +
-                        $"<div style=\"overflow: hidden;width:70px;height: 70px;border-radius: 50%;\">" +
-                        $"<img style=\"width: 100%;height: 100%;object-fit: cover;\" src = \"{boss.Picture}\">" +
-                        $"</div>" +
-                        $"</p>" +
-                        $"Boss Id: {boss.Id}" +
-                        $"<hr>");
+            {"{bosses}", bossesHtml},
+            {"{levels}", levelsHtml},
+            {"{friends}", friendHtml},
+        };
+        
+        var htmlPage = HtmlFileManager.GetHtmlFileBody(StaticFilePaths.HomeFilesPath, StaticFilePaths.MainPageName);
+
+        foreach (var placeholder in globalPlaceholders)
+        {
+            htmlPage = htmlPage.Replace(placeholder.Key, placeholder.Value);
         }
 
-        return html.ToString();
+        return htmlPage;
     }
 
     [Get("AddBoss")]
     public void AddBoss(Boss boss)
     {
         // Console.WriteLine(boss.Name);
-        _repo.Add(boss);
+        _bosses.Add(boss);
     }
     
     [Get("DeleteBoss")]
     public async Task DeleteBoss(string id)
     {
         // Console.WriteLine(id);
-        _repo.Delete(id);
+        _bosses.Delete(id);
     }
     
     [Get("EditBoss")]
     public void EditBoss(Boss boss)
     {
         // Console.WriteLine(boss.Name);
-        _repo.Edit(boss);
+        _bosses.Edit(boss);
+    }
+
+
+    private string GetBossesHtml()
+    {
+        var bosses = _bosses.Select().Result;
+
+        var html = new StringBuilder();
+        
+        foreach (var boss in bosses.OrderBy(b => b.Id))
+        {
+            var bossTemplate = 
+                HtmlFileManager.GetHtmlFileBody(StaticFilePaths.HomeTemplatesPath,
+                    StaticFilePaths.MainPageBossTemplate);
+
+            var placeholders = new Dictionary<string, string>();
+
+            placeholders["{bossId}"] = boss.Id.ToString();
+            placeholders["{bossName}"] = boss.Name;
+            placeholders["{bossPicture}"] = boss.Picture;
+
+            foreach (var placeholder in placeholders)
+            {
+                bossTemplate = bossTemplate.Replace(placeholder.Key, placeholder.Value);
+            }
+
+            html.Append(bossTemplate);
+        }
+
+        return html.ToString();
+    }
+
+    private string GetLevelsHtml()
+    {
+        var levels = _levels.Select().Result;
+        
+        var html = new StringBuilder();
+        
+        foreach (var level in levels.OrderBy(b => b.Id))
+        {
+            var levelTemplate = 
+                HtmlFileManager.GetHtmlFileBody(StaticFilePaths.HomeTemplatesPath,
+                    StaticFilePaths.MainPageLevelTemplate);
+
+            var placeholders = new Dictionary<string, string>();
+
+            placeholders["{levelName}"] = level.Name;
+            placeholders["{levelPicture}"] = level.Picture;
+
+            foreach (var placeholder in placeholders)
+            {
+               levelTemplate = levelTemplate.Replace(placeholder.Key, placeholder.Value);
+            }
+
+            html.Append(levelTemplate);
+        }
+
+        return html.ToString();
+    }
+
+    private string GetFriendsHtml()
+    {
+        var friends = _friends.Select().Result;
+        
+        var html = new StringBuilder();
+        
+        foreach (var friend in friends.OrderBy(b => b.Id))
+        {
+            var friendTemplate = 
+                HtmlFileManager.GetHtmlFileBody(StaticFilePaths.HomeTemplatesPath,
+                    StaticFilePaths.MainPageFriendTemplate);
+
+            var placeholders = new Dictionary<string, string>();
+
+            placeholders["{friendPicture}"] = friend.Picture;
+            placeholders["{friendLink}"] = friend.Link;
+
+            foreach (var placeholder in placeholders)
+            {
+                friendTemplate = friendTemplate.Replace(placeholder.Key, placeholder.Value);
+            }
+
+            html.Append(friendTemplate);
+        }
+
+        return html.ToString();
     }
 }
